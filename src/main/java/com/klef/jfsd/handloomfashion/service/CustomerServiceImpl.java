1d31c39554b3007f7f443902ab62c5f91ca02b2d
Indukuri Aashritha <aashrithaindukuri@gmail.com>
2025-04-27 23:48:38 +0530

    V1.0

---
+++ src/main/java/com/klef/jfsd/handloomfashion/service/CustomerServiceImpl.java
@@ -0,0 +1,68 @@
+package com.klef.jfsd.handloomfashion.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.klef.jfsd.handloomfashion.model.Customer;
+import com.klef.jfsd.handloomfashion.repository.CustomerRepository;
+
+@Service
+public class CustomerServiceImpl implements CustomerService 
+{
+
+	@Autowired
+	private CustomerRepository repository;
+	
+	
+	@Override
+	public String signup(Customer c) {
+		repository.save(c);
+		return "Customer Added Successfully";
+	}
+
+
+	@Override
+	public Customer login(String username, String password) {
+	        return repository.findById(username)
+	                .filter(c -> c.getPassword().equals(password))
+	                .orElse(null);
+	    }
+
+
+	@Override
+	public Iterable<Customer> getAllCustomers() {
+		return repository.findAll();
+	}
+
+
+	 @Override
+	    public Customer updateCustomer(String username, Customer c) {
+	        Customer existingCustomer = repository.findById(username).orElse(null);
+	        if (existingCustomer != null) {
+	            existingCustomer.setEmail(c.getEmail());
+	            existingCustomer.setPassword(c.getPassword());
+	            existingCustomer.setMobile(c.getMobile());
+	            repository.save(existingCustomer);
+	            return existingCustomer;
+	        }
+	        return null; 
+	    }
+
+
+	 @Override
+	    public String deletecustomer(String username) {
+	        Customer existingCustomer = repository.findById(username).orElse(null);
+	        if (existingCustomer != null) {
+	            // Delete the customer if found
+	            repository.delete(existingCustomer);
+	            return "Customer deleted successfully";
+	        }
+	        return "Customer not found"; // If customer does not exist
+	    }
+	
+	
+	
+	
+
+}
